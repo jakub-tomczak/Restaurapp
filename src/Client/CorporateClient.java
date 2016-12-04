c090ed344659df3fe7820f62abb5d9f42bbf0fe8
Jakub Tomczak <jakub.tomczak@live.com>
2016-12-04 21:23:50 +0100

    Add fields and constructors to CorporateClient class.

--- src/Client/CorporateClient.java
+++ src/Client/CorporateClient.java
@@ -4,4 +4,24 @@ package Client;
  * Created by Jakub on 04.12.2016.
  */
 public class CorporateClient extends Client {
+    private Address letterAddress;
+    private String bankAccountNumber;
+    private String REGON;
+
+    public CorporateClient() {
+        letterAddress = null;
+        bankAccountNumber = null;
+        REGON = null;
+    }
+
+    public CorporateClient(String bankAccountNumber, String REGON) {
+        this.bankAccountNumber = bankAccountNumber;
+        this.REGON = REGON;
+        this.letterAddress = address;
+    }
+
+    public CorporateClient(Address letterAddress, String bankAccountNumber, String REGON) {
+        this(bankAccountNumber, REGON);
+        this.letterAddress = letterAddress;
+    }
 }
